161e3b95324b206e2356938d118eb7041a423c06
Abdelrahman <abdelrhmanaltawil@outlook.com>
2023-01-03 15:41:20 +0300

    move out data splitting node

--- src/multiple_sclerosis/pipelines/data_processing/pipeline.py
+++ src/multiple_sclerosis/pipelines/data_processing/pipeline.py
@@ -7,13 +7,12 @@ generated using Kedro 0.18.3
 from kedro.pipeline import Pipeline, node, pipeline
 
 # local imports
-from multiple_sclerosis.nodes.data_nodes import extract_data, clean, split
+from multiple_sclerosis.pipelines.data_processing.nodes import extract_data, clean_data
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
-            # data nodes
             node(
                 func=extract_data,
                 inputs=["2min_walking_test_data", "parameters"],
@@ -21,16 +20,10 @@ def create_pipeline(**kwargs) -> Pipeline:
                 name="data_extraction",
             ),
             node(
-                func=clean,
+                func=clean_data,
                 inputs=["modeling_data", "parameters"],
                 outputs="cleaned_data",
                 name="data_cleaning",
             ),
-            node(
-                func=split,
-                inputs=["cleaned_data", "parameters"],
-                outputs=["X_train", "X_test", "y_train", "y_test"],
-                name="data_splitting",
-            )
         ]
     )
