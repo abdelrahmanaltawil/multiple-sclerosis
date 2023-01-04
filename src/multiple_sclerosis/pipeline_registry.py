f464b72d48916b3f8a467daeab160928542635f9
Abdelrahman <abdelrhmanaltawil@outlook.com>
2023-01-04 12:01:29 +0300

    experimenting with model registry

--- src/multiple_sclerosis/pipeline_registry.py
+++ src/multiple_sclerosis/pipeline_registry.py
@@ -1,8 +1,13 @@
 """Project pipelines."""
-from typing import Dict
 
+# env imports
+from typing import Dict
 from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
+
+# local imports
+import multiple_sclerosis.pipelines.data_processing.pipeline as dp
+import multiple_sclerosis.pipelines.data_science.pipeline as ds
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -11,6 +16,15 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
-    return pipelines
+    # pipelines = find_pipelines()
+    # pipelines["__default__"] = sum(pipelines.values())
+    # return pipelines
+
+    data_processing_pipeline = dp.create_pipeline()
+    data_science_pipeline = ds.create_pipeline()
+
+    return {
+        "__default__": data_processing_pipeline + data_science_pipeline,
+        "data_processing": data_processing_pipeline,
+        "data_science": data_science_pipeline,
+    }
