2fcf4dc158c042e1adc8b31c252d5f57a787153e
Abdelrahman <abdelrhmanaltawil@outlook.com>
2023-02-17 18:38:01 +0100

    revert of up to date optimizers

--- src/multiple_sclerosis/pipelines/data_science/helpers/optimizers.py
+++ src/multiple_sclerosis/pipelines/data_science/helpers/optimizers.py
@@ -8,14 +8,14 @@ def get_optimizer(optimizer: str, learning_rate: float) -> tf.keras.optimizers:
     '''
 
     tensorflow_optimizers = {
-        "Adadelta"  : tf.keras.optimizers.legacy.Adadelta(learning_rate= learning_rate),
-        "RMSprop"   : tf.keras.optimizers.legacy.RMSprop(learning_rate= learning_rate),
-        "Adagrad"   : tf.keras.optimizers.legacy.Adagrad(learning_rate= learning_rate),
-        "Adamax"    : tf.keras.optimizers.legacy.Adamax(learning_rate= learning_rate),
-        "Nadam"     : tf.keras.optimizers.legacy.Nadam(learning_rate= learning_rate),
-        "Adam"      : tf.keras.optimizers.legacy.Adam(learning_rate= learning_rate),
-        "Ftrl"      : tf.keras.optimizers.legacy.Ftrl(learning_rate= learning_rate),
-        "SGD"       : tf.keras.optimizers.legacy.SGD(learning_rate= learning_rate)
+        "Adadelta"  : tf.keras.optimizers.Adadelta(learning_rate= learning_rate),
+        "RMSprop"   : tf.keras.optimizers.RMSprop(learning_rate= learning_rate),
+        "Adagrad"   : tf.keras.optimizers.Adagrad(learning_rate= learning_rate),
+        "Adamax"    : tf.keras.optimizers.Adamax(learning_rate= learning_rate),
+        "Nadam"     : tf.keras.optimizers.Nadam(learning_rate= learning_rate),
+        "Adam"      : tf.keras.optimizers.Adam(learning_rate= learning_rate),
+        "Ftrl"      : tf.keras.optimizers.Ftrl(learning_rate= learning_rate),
+        "SGD"       : tf.keras.optimizers.SGD(learning_rate= learning_rate)
     }
 
     if optimizer not in tensorflow_optimizers.keys(): raise NotImplemented("optimizer not implemented")
